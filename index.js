c427c093687a9903b085e1fc33cec38495ae6dca
Aniket <aniketpratapsingh7310@gmail.com>
2025-07-30 11:53:31 +0530

    code format

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./src/myconfigs/db');    
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./src/myconfigs/db");
 
-
-const cors = require('cors');
-
-
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
@@ -13,29 +10,25 @@ const app = express();
 // Database connection
 connectDB();
 
-
 // Allow requests from http://localhost:3000
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     credentials: true, // Allow cookies if needed
-  }));
+  })
+);
 // Middleware
 app.use(express.json());
-console.log("i am her eno")
+console.log("i am her eno");
 
 app.use(express.urlencoded({ extended: true }));
-console.log("i am her two")
+console.log("i am her two");
 
 const categoryRoutes = require("./routes/category");
 app.use("/api/categories", categoryRoutes);
 
-
-
-
-
-console.log("i am her jwlnce")
-
+console.log("i am her jwlnce");
 
 const PORT = process.env.PORT || 9000;
 
